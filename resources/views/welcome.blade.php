061e3b34a8e337cd36a18b9f1c05299b94e0b34a
unknown <nebir1993@gmail.com>
2018-07-12 12:03:22 +0600

    welcome page, registration completed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,67 +5,19 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Examination Paper Status</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        {{--<link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">--}}
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
+        {{--  Default Laravel Template --}}
+        {{--<div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
@@ -79,7 +31,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Examination <br> Paper Status
                 </div>
 
                 <div class="links">
@@ -90,6 +42,26 @@
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
-        </div>
+        </div>--}}
+
+        <header class="welcome-page-header">
+            <div class="overlay"></div>
+            <div class="header-wrapper">
+                <div class="header-wrapper-inner">
+                    <div class="container">
+                        <div class="row align-items-center">
+                            <div class="col-12 col-lg-6 offset-lg-3 text-center ">
+                                <h1 class="page-title"><span>Examination</span> Paper Status</h1>
+                                <a class="btn btn-eps  btn-eps-default btn-eps-transparent" href="{{ route('login') }}">Login to continue</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+        {{-- Scripts --}}
+        <script src="{{ asset('js/jquery-3.3.1.min.js') }}" defer></script>
+        <script src="{{ asset('js/bootstrap.min.js') }}" defer></script>
     </body>
 </html>
